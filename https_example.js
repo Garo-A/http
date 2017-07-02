d1101ab05a14b9ec3f230c0549f0a09ebc698c69
Garo <garo.assilian@gmail.com>
2017-07-02 16:44:34 +0000

    Requesting example.org

--- https_example.js
+++ https_example.js
@@ -1,1 +1,18 @@
 var https = require ('https');
+
+var options = {
+  host: "www.example.org",
+  path: "/"
+}
+
+// called by https when request is made
+
+callback = function() {
+  console.log('in Response handler callback!')
+};
+
+console.log("I'm about to make a request");
+
+https.request(options, callback).end();
+
+console.log("I've made the request");
