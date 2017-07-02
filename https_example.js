af0899111a36899c29853cddbf62166a5f9af682
Garo <garo.assilian@gmail.com>
2017-07-02 16:53:50 +0000

    Changing callback function to correctly output HTML response using streams

--- https_example.js
+++ https_example.js
@@ -9,7 +9,12 @@ var options = {
 
 callback = function(response) {
   console.log('in Response handler callback!') //This is basically called when the request has been completed.
-  console.log("Response: ",response);
+
+  response.on('data', function(chunk){ //don't forget to define what you want the callback to run on.
+    console.log("-- CHUNK LENGTH " + chunk.length + " --") //shows the length of the chunk I'm trying to get
+    console.log(chunk.toString()); //Turns the chunk into a string and outputs.
+  })
+// console.log("Response: ",response); // This is used to actually LOG the response. This is actually a stream.
 };
 
 console.log("I'm about to make a request");
